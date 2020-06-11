8c77c9dd94b21fa6f232efaa689b6fd250071dc5
kherld-hussein <eternety11@gmail.com>
2020-06-12 01:30:37 +0300

    Add copy functionality

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 // alert("Keep calm and git push")
 
+var copyButton = document.querySelectorAll(".btn-clipboard").length;
+
+for (var i = 0; i < copyButton; i++) {
+  // const element = array[i];
+
+
+document.querySelectorAll(".btn-clipboard")[i].addEventListener("click", function () {
+  alert("copied!");
+});
+  
+}
+
+
+
 $("h6").addClass("glow-anime");
 setTimeout(function() {
   $("h6").addClass("glow-anime");
 }, 200);
 
-function name(params) {
-  
-}
+
 
